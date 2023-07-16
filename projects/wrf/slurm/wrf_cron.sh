53931637338f04f6ead337ef3f84d2e0c41cc903
jordancaraballo <jordan.a.caraballo-vega@nasa.gov>
2023-07-16 09:23:52 -0400

    Adding general cron job

--- projects/wrf/slurm/wrf_cron.sh
+++ projects/wrf/slurm/wrf_cron.sh
@@ -4,14 +4,14 @@
 #SBATCH -N 1
 #SBATCH --mail-user=jordan.a.caraballo-vega@nasa.gov
 #SBATCH --mail-type=ALL
-#SBATCH --output=/explore/nobackup/people/jacaraba/development/wildfire-occurrence/projects/wrf/slurm/daily-wrf-%x.%j.out
-#SBATCH --error=/explore/nobackup/people/jacaraba/development/wildfire-occurrence/projects/wrf/slurm/daily-wrf-%x.%j.err
+#SBATCH --output=/explore/nobackup/projects/ilab/projects/LobodaTFO/operations/wildfire-occurrence/projects/wrf/slurm/daily-wrf-%x.%j.out
+#SBATCH --error=/explore/nobackup/projects/ilab/projects/LobodaTFO/operations/wildfire-occurrence/projects/wrf/slurm/daily-wrf-%x.%j.err
 
 # Daily WRF cron job slurm submission
 
 eval "$(conda shell.bash hook)"
 
 conda activate wrf-development;
-export PYTHONPATH="/explore/nobackup/people/$USER/development/wildfire-occurrence"
+export PYTHONPATH="/explore/nobackup/projects/ilab/projects/LobodaTFO/operations/wildfire-occurrence"
 
-srun -n 1 python /explore/nobackup/people/$USER/development/wildfire-occurrence/wildfire_occurrence/view/wrf_pipeline_cli.py -c /explore/nobackup/people/$USER/development/wildfire-occurrence/wildfire_occurrence/templates/config.yaml --forecast-lenght 10 --pipeline-step all
+srun -n 1 python /explore/nobackup/projects/ilab/projects/LobodaTFO/operations/wildfire-occurrence/wildfire_occurrence/view/wrf_pipeline_cli.py -c /explore/nobackup/projects/ilab/projects/LobodaTFO/operations/wildfire-occurrence/wildfire_occurrence/templates/config.yaml --forecast-lenght 10 --pipeline-step all
